1335c87019fc038e4e78f0d894b7d19c32eebbd8
dr9170 <47762004+dr9170@users.noreply.github.com>
2019-04-28 04:11:07 -0500

    francia

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id prueba asterisk:" . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "id prueba francia:" . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
     }
 } else {
     echo "0 results";
